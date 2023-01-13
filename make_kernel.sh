e5698be78bdcd2f9939dd4e4cb2963c62b9dc3b3
ncer <huangzzk@bupt.edu.cn>
2023-01-13 22:26:23 +0800

    edit script
    Signed-off-by: ncer <huangzzk@bupt.edu.cn>

--- make_kernel.sh
+++ make_kernel.sh
@@ -1,2 +1,2 @@
-#CROSS_COMPILE=/home/ncer/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-  ARCH=arm make pipx4_v3s_defconfig #licheepi_zero_defconfig
+#CROSS_COMPILE=/home/ncer/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-  ARCH=arm make licheepi_zero_defconfig
 CROSS_COMPILE=/home/ncer/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-  ARCH=arm make -j7
